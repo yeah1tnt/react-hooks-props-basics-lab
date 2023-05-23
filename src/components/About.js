5980df1b4b0b10363247932c7942155ce2e58698
Tong Tang <pichip22@hotmail.com>
2023-05-23 06:39:53 -0400

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Links from "./Links.js"
 
-function About() {
+
+
+function About(props) {
+  if(!props.bio || props.bio.trim()=== "") {
+    return null;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links
+        github={props.github}
+        linkedin = {props.linkedin}/>
     </div>
   );
 }
